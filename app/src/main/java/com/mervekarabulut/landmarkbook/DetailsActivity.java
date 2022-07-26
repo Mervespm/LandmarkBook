a5f15b5f51b4092f5d3dd6bdaee5e424fab8f53c
Merve <merve@Merve-MacBook-Air-2.local>
2022-07-27 00:05:48 +0300

    Initial commit

---
+++ app/src/main/java/com/mervekarabulut/landmarkbook/DetailsActivity.java
@@ -0,0 +1,39 @@
+package com.mervekarabulut.landmarkbook;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mervekarabulut.landmarkbook.databinding.ActivityDetailBinding;
+import com.mervekarabulut.landmarkbook.databinding.ActivityMainBinding;
+
+import org.w3c.dom.Text;
+
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private ActivityDetailBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        //Landmark selectedLandmark = (Landmark) intent.getSerializableExtra("landmark");
+
+        Singleton singleton = Singleton.getInstance();
+        Landmark selectedLandmark = singleton.getSelectedLandmark();
+
+        binding.landmarkNameText.setText(selectedLandmark.name);
+        binding.countryNameText.setText(selectedLandmark.country);
+        binding.imageView.setImageResource(selectedLandmark.image);
+
+    }
+}
